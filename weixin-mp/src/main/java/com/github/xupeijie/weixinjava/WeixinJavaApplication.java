4fdd6c7ea01f3832d780b99a82f6fa51ffac17a6
xupeijie <xupeijie@pekon.com>
2019-07-10 14:18:46 +0800

    add

---
+++ weixin-mp/src/main/java/com/github/xupeijie/weixinjava/WeixinJavaApplication.java
@@ -0,0 +1,13 @@
+package com.github.xupeijie.weixinjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeixinJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WeixinJavaApplication.class, args);
+    }
+
+}
